3c2e40e4945c5e25058f4b4d36097c8ad4927774
monica <monicaseeds@outlook.com>
2023-08-25 17:25:39 +0800

    review the app code

--- javafx-kotlin-maven-archetype/src/main/resources/archetype-resources/src/main/kotlin/AppLaunch.kt
+++ javafx-kotlin-maven-archetype/src/main/resources/archetype-resources/src/main/kotlin/AppLaunch.kt
@@ -8,7 +8,6 @@ import javafx.scene.control.Alert
 import javafx.scene.control.Button
 import javafx.scene.control.PasswordField
 import javafx.scene.control.TextField
-import javafx.scene.input.MouseEvent
 import javafx.scene.layout.Background
 import javafx.scene.layout.GridPane
 import javafx.scene.layout.VBox
@@ -17,27 +16,31 @@ import javafx.stage.Stage
 
 class AppLaunch : Application() {
     override fun start(stage: Stage) {
-        val pane = GridPane()
-        pane.alignment = Pos.CENTER
-        pane.setPrefSize(820.0, 560.0)
-        pane.background = Background.fill(Paint.valueOf("#FAEBD7"))
-        val row = VBox()
-        row.alignment = Pos.CENTER
-        row.background = Background.fill(Paint.valueOf("#A69532"))
-        val username = TextField()
-        username.setPrefSize(320.0, 32.0)
-        val password = PasswordField()
-        password.setPrefSize(320.0, 32.0)
-        val btn1 = Button()
-        btn1.setPrefSize(320.0, 46.0)
-        btn1.onMouseClicked = EventHandler {
-            Alert(Alert.AlertType.CONFIRMATION).show()
-        }
-        row.children.addAll(username, password, btn1)
-        pane.children.add(row)
-        val scene = Scene(pane)
-        stage.setScene(scene)
-        stage.show()
+        stage.apply {
+            this.scene = Scene(GridPane().apply {
+                this.alignment = Pos.CENTER
+                this.setPrefSize(820.0, 560.0)
+                this.background = Background.fill(Paint.valueOf("#FAEBD7"))
+                this.children.add(VBox().apply {
+                    this.alignment = Pos.CENTER
+                    this.background = Background.fill(Paint.valueOf("#A69532"))
+                    this.children.addAll(
+                        TextField().apply {
+                            this.setPrefSize(320.0, 32.0)
+                        },
+                        PasswordField().apply {
+                            this.setPrefSize(320.0, 32.0)
+                        },
+                        Button().apply {
+                            this.setPrefSize(320.0, 46.0)
+                            this.onMouseClicked = EventHandler {
+                                Alert(Alert.AlertType.CONFIRMATION).show()
+                            }
+                        }
+                    )
+                })
+            })
+        }.show()
     }
 
 }
